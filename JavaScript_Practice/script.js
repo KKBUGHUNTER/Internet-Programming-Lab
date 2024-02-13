29f94d30e22eb22c62ea9b0fc2a78d844fc4c6fd
karthikeyan <karthikeyan.aas@gmail.com>
2024-02-13 21:30:29 +0530

    45min

---
+++ JavaScript_Practice/script.js
@@ -0,0 +1,26 @@
+function func1() {
+    document.getElementById('heading').innerHTML = "welcome to JavaScript";
+    document.write("<br> <hr> ");
+    
+}
+
+function func2(){
+    var b = window.prompt("Enter the value 1:");
+    window.alert("Value of B = " + b);
+}
+
+var a = 10;
+document.write(a + " Type: " + typeof(a) +'<br>');
+a = "hello world";
+document.write(a + " Type: " + typeof(a) +'<br>');
+a = 12.45;
+document.write(a + " Type: " + typeof(a) +'<br>');
+a = true;
+document.write(a + " Type: " + typeof(a) +'<br>');
+var b;
+document.write(b + " Type: " + typeof(b) +'<br>');
+
+
+
+
+
