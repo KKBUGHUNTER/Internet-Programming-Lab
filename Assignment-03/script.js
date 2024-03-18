2aac525ee56085f78eb755b41139e27bdef8db4d
karthikeyan <karthikeyan.aas@gmail.com>
2024-03-18 23:24:44 +0530

    memorygame

--- Assignment-03/script.js
+++ Assignment-03/script.js
@@ -1,10 +1,41 @@
-const numbers = ["1️⃣","1️⃣","2️⃣","2️⃣","3️⃣","3️⃣","4️⃣","4️⃣","5️⃣","5️⃣","6️⃣","6️⃣","7️⃣","7️⃣","8️⃣","8️⃣"];
-function generate_board(){
-    var shuf_number = numbers.sort(() => (Math.random() > .5)?2:-1);
-    for(var i=0; i<numbers.length; i++){
+const emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣","1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣"];
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function generate_board() {
+    const shuffledEmojis = shuffle(emojis);
+    for (let i = 0; i < shuffledEmojis.length; i++) {
         let box = document.createElement('div');
         box.className = 'item';
-        box.innerHTML = shuf_number[i];
+        box.innerHTML = shuffledEmojis[i];
+        box.onclick = function () {
+            this.classList.add('boxOpen');
+            setTimeout(function () {
+                if (document.querySelectorAll('.boxOpen').length > 1) {
+                    if (document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML) {
+                        document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch');
+                        document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch');
+
+                        document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen');
+                        document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen');
+
+                        
+                        if (document.querySelectorAll('.boxMatch').length == emojis.length) {
+                            alert('Win');
+                        } 
+                    } else {
+                        document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen');
+                         document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen');
+                    }
+                }
+            }, 300);
+        };
         document.querySelector('.game').appendChild(box);
     }
 }
--- EventMan/SqlQuery.sql
+++ EventMan/SqlQuery.sql
@@ -2,47 +2,79 @@ CREATE DATABASE eventman;
 
 USE eventman;
 
-CREATE TABLE users(
-    userid VARCHAR2(16) CONSTRAINT users_PF PRIMARY KEY,
-    username VARCHAR2(255) NOT NULL,
-    passwd VARCHAR2(255) NOT NULL,
-    sessionkey VARCHAR2(255)
+CREATE TABLE organization (
+    orgname VARCHAR(255) PRIMARY KEY, 
+    orgstate VARCHAR(127),
+    orgtype VARCHAR(127)
 );
 
-CREATE TABLE organization(
-    orgid VARCHAR2(16) CONSTRAINT org_PK PRIMARY KEY,
-    orgname VARCHAR2(255) NOT NULL, 
-    orgstate VARCHAR2(127),
-    orgtype VARCHAR2(127)
+CREATE TABLE users (
+    phone INT PRIMARY KEY,
+    uname VARCHAR(255) NOT NULL,
+    passwd VARCHAR(255) NOT NULL,
+    position VARCHAR(127) NOT NULL,
+    orgname VARCHAR(255),
+    CONSTRAINT user_org_FK FOREIGN KEY (orgname) REFERENCES organization(orgname)
 );
 
-CREATE TABLE user(
-    userid VARCHAR2(16) CONSTRAINT user_PF PRIMARY KEY,
-    orgid VARCHAR2(16) CONSTRAINT user_org_FK FOREIGN KEY (orgid) REFERENCES organization(orgid),
-    uname VARCHAR2(255) NOT NULL,
-    position VARCHAR2(127) NOT NULL,
-);
-
-CREATE TABLE events(
-    eid VARCHAR2(16) CONSTRAINT envet_PK PRIMARY KEY,
-    orgid VARCHAR2(16) CONSTRAINT event_org_FK FOREIGN KEY (orgid) REFERENCES organization(orgid),
-    eventhead VARCHAR2(255) CONSTRAINT event_user_FK FOREIGN KEY (eventhead) REFERENCES user(userid), 
-    ename VARCHAR2(255) NOT NULL,
-    etype VARCHAR2(255),
-    sdate DATE NOT NULL,  -- - '2024-03-18'
+CREATE TABLE events (
+    ename VARCHAR(255) PRIMARY KEY,
+    orgname VARCHAR(255),
+    eventhead INT NOT NULL,
+    etype VARCHAR(255),
+    sdate DATE NOT NULL,
     edate DATE NOT NULL,
+    CONSTRAINT event_org_FK FOREIGN KEY (orgname) REFERENCES organization(orgname),
+    CONSTRAINT event_user_FK FOREIGN KEY (eventhead) REFERENCES users(phone)
 );
 
-CREATE TABLE organisens(
-    eid VARCHAR2(16) CONSTRAINT ors_eid_FK FOREIGN KEY (eid) REFERENCES events(eid),
-    userid VARCHAR2(16) CONSTRAINT ors_userid_FK FOREIGN KEY (userid) REFERENCES user(userid),
-    position VARCHAR2(255) NOT NULL,
-    CONSTRAINT PK_organisens PRIMARY KEY (eid, userid)
+CREATE TABLE organisens (
+    ename VARCHAR(16),
+    phone INT,
+    position VARCHAR(255) NOT NULL,
+    CONSTRAINT PK_organisens PRIMARY KEY (ename, phone),
+    CONSTRAINT ors_eid_FK FOREIGN KEY (ename) REFERENCES events(ename),
+    CONSTRAINT ors_userid_FK FOREIGN KEY (phone) REFERENCES users(phone)
 );
 
-CREATE TABLE participants(
-    eid VARCHAR2(16) CONSTRAINT ors_eid_FK FOREIGN KEY (eid) REFERENCES events(eid),
-    pname VARCHAR2(255) NOT NULL,
-    porg VARCHAR2(255),
-    pstatus VARCHAR2(255)
+CREATE TABLE participants (
+    ename VARCHAR(16),
+    pname VARCHAR(255) NOT NULL,
+    porg VARCHAR(255),
+    pstatus VARCHAR(255),
+    CONSTRAINT part_eid_FK FOREIGN KEY (ename) REFERENCES events(ename)
 );
+
+DROP TABLE participants, organisens, events ,users, organization;
+
+INSERT INTO organization VALUES('SSN', 'Engineering', 'college');
+INSERT INTO organization VALUES('IET', 'Devemopment', 'company');
+
+INSERT INTO users VALUES (1,'karthikeyan', '12345','student','SSN');
+INSERT INTO users VALUES (2,'Ashwin', 'ashwin','student','SSN');
+INSERT INTO users VALUES (3,'Iray', 'iray','student','SSN');
+INSERT INTO users VALUES (4,'Mega', 'mega','Developer','IET');
+INSERT INTO users VALUES (5,'Magesh', 'magesh','Developer','IET');
+
+INSERT INTO events VALUES('Hack the Box','SSN',1,'Tech','2024-03-18','2024-04-18');
+INSERT INTO events VALUES('Hack To Hire','IET',4,'Non-Tech','2024-01-08','2024-03-28');
+
+INSERT INTO organisens VALUES('Hack the Box', 1,'Head');
+INSERT INTO organisens VALUES('Hack the Box', 2,'Team Lead');
+INSERT INTO organisens VALUES('Hack To Hire', 3,'Team Lead');
+INSERT INTO organisens VALUES('Hack To Hire', 4,'Head');
+
+INSERT INTO participants VALUES('Hack the Box','Sam','self','PAID');
+INSERT INTO participants VALUES('Hack the Box','Som','self','PAID');
+INSERT INTO participants VALUES('Hack the Box','Ram','self','NOT PAID');
+INSERT INTO participants VALUES('Hack the Box','Rahul','self','PAID');
+INSERT INTO participants VALUES('Hack the Box','Ravi','self','NOT PAID');
+INSERT INTO participants VALUES('Hack To Hire','Muthu','college','PAID');
+INSERT INTO participants VALUES('Hack To Hire','Danesh','college','PAID');
+INSERT INTO participants VALUES('Hack To Hire','Hari','college','NOT PAID');
+
+SELECT * FROM organization;
+SELECT * FROM users;
+SELECT * FROM events;
+SELECT * FROM organisens;
+SELECT * FROM participants;
