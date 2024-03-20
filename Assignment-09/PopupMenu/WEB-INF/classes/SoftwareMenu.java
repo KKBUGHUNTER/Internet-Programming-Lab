f748a2a7a907dbd39a8ac3805f7fa460ad4dc25a
karthikeyan <karthikeyan.aas@gmail.com>
2024-03-20 13:56:40 +0530

    Update

--- Assignment-09/PopupMenu/WEB-INF/classes/SoftwareMenu.java
+++ Assignment-09/PopupMenu/WEB-INF/classes/SoftwareMenu.java
@@ -1,20 +1,38 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.sql.*;
 
 public class SoftwareMenu extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Error loading MySQL JDBC driver", e);
+        }
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        String[] software = { "Git", "VScode" };
+        try (
+                Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/courses", "root",
+                        "password");
+                Statement stmt = conn.createStatement();) {
+            String sql = "SELECT toolname FROM development WHERE type='software'";
+            ResultSet result = stmt.executeQuery(sql);
+            int rowCount = 0;
 
-        out.println("<ol>");
-        for (int i = 0; i < software.length; i++) {
-            out.println("<li>" + software[i] + "</li>");
+            while (result.next()) {
+                String toolName = result.getString("toolname");
+                out.println("<p>" + toolName + " </p>");
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
         }
-        out.println("</ol>");
+
         out.close();
     }
 }
