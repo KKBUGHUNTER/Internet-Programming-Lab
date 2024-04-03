c1cddce8bf27ca89b598a22ed2bdc5fc0301df9f
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2024-04-03 11:49:20 +0530

    Add files via upload

---
+++ Lab_Test-01/sample/src/App.js
@@ -0,0 +1,35 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>GOALS in Life</h3>
+        <p>These are my list goals to achieve in life.</p>
+        <ol>
+          <li>Build a Big management Web Application.</li>
+          <li>That want to use by 10M users.</li>
+        </ol>
+        <h3>Habbies</h3>
+        <ul>
+          <li>Learning Computer Science.</li>
+          <li>Learning Guitar.</li>
+        </ul>
+
+    <h3>Favorit Food</h3>
+        <ul>
+          <li>Dal and Rice</li>
+        </ul>
+
+        <h3>Place Wish to Visi:</h3>
+        <a href="https://about.google/intl/ALL_in/locations/?region=north-america&office=mountain-view">Google Head office</a>
+        <pre>       Mountain View is a city in Santa Clara County, California, United States, part of the San Francisco Bay Area. Named for its views of the Santa Cruz Mountains, it has a population of 82,376 as of the 2020 census</pre>
+        <br></br> 
+        <a href="https://maps.apple.com/place?auid=559098170073364042">Apple Park</a> 
+        <pre>      Apple Inc. is an American multinational technology company headquartered in Cupertino, California that designs, develops, and sells consumer electronics, ...</pre>
+      </header>
+    </div>
+  );
+}
+
+export default App;
